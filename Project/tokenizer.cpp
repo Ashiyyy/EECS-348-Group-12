57c9084d684b1445cb2d36caf39eee3e692248f7
kucaleb <caleb.neil@ku.edu>
2024-11-21 14:45:35 -0600

    Added a way to validate the input

--- Project/tokenizer.cpp
+++ Project/tokenizer.cpp
@@ -5,9 +5,47 @@ using namespace std;
 class Tokenizer {
     public:
         vector<char> tokens;
+        string input;
+
         Tokenizer(string s) {
-            for (char c: s) {
+            input = s;
+        }
+
+        vector<char> getTokens() {
+            for (char c: input) {
                 tokens.push_back(c);
             }
+            return tokens;
+        }
+
+        bool isValid() {
+            string legal = "1234567890()*/+-";
+            string operators = "*()-+/";
+            bool valid = true;
+            int t = string::npos;
+            int i;
+            int count = 0;
+
+            //checks that every char is valid in a math expression
+            try {
+                for(i = 0; i < input.length(); i++) {
+                    char c = input.substr(i, 1)[0];
+                    if(legal.find(c) == t) {
+                        valid = false;
+                    } else if (operators.find(c) == t && operators.find(input.substr(i + 1, 1)[0] == t)) {
+                        valid = false;
+                    } else if (c == '(') {
+                        count++;
+                    } else if (c == ')') {
+                        count--;
+                    }
+                }
+                if (count != 0) {
+                    valid = false;
+                }
+            } catch(...) {
+                valid = false;
+            }
+            return valid;
         }
 };
